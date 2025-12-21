26f81cc34ea1878c7130c0d7a2beaba5399e9505
Murzik666 <petyok.murzin75@gmail.com>
2025-12-21 18:02:25 +0300

    Проверка 7 исправление 2кб

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import { uploadFile } from '../controllers/upload'
-import fileMiddleware from '../middlewares/file'
+import { uploadSingle } from '../middlewares/file'
 
 const uploadRouter = Router()
-uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
+uploadRouter.post('/', uploadSingle('file'), uploadFile)
 
 export default uploadRouter
